df91303b7087abe1f86e23194df4485a2c7193fd
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-05 13:38:28 +0100

    Initial structure

--- specs/user_registration_spec.py
+++ specs/user_registration_spec.py
@@ -1,18 +1,30 @@
 
 from expects import expect, be_true, raise_error
 
+class UserService(object):
+
+    def register(self, nickname):
+        pass
+
+    def is_registered(self, nickname):
+        pass
+
+def create_user_service():
+    return UserService()
+
 with description('Register user'):
 
     with before.each:
         self.nickname = '@foolano'
 
     with it('registers a new user'):
-        # Register a user
-        # Validate that the user is registered
-        pass
+        user_service = create_user_service()
+
+        user_service.register(self.nickname)
+
+        expect(user_service.is_registered(self.nickname)).to(be_true)
 
     with context('when user already exists'):
         with it('raises error'):
             # Register a user
-            #
             pass
