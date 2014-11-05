da2df2812796454e7878fb7ed00bec08488e322e
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-05 13:50:26 +0100

    Error when a user already exists

--- specs/user_registration_spec.py
+++ specs/user_registration_spec.py
@@ -1,11 +1,16 @@
 
 from expects import expect, be_true, be_false, raise_error
 
+class UserAlreadyRegisteredError(Exception):
+    pass
+
 class UserService(object):
     def __init__(self):
         self._users = set()
 
     def register(self, nickname):
+        if self.is_registered(nickname):
+            raise UserAlreadyRegisteredError()
         self._users.add(nickname)
 
     def is_registered(self, nickname):
@@ -29,7 +34,8 @@ with description('Register user'):
 
         expect(self.user_service.is_registered(self.nickname)).to(be_true)
 
-    with _context('when user already exists'):
+    with context('when user already exists'):
         with it('raises error'):
-            # Register a user
-            pass
+            self.user_service.register(self.nickname)
+
+            expect(lambda: self.user_service.register(self.nickname)).to(raise_error(UserAlreadyRegisteredError))
