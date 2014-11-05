ee5a04115476d20e6b71ebdd147c2656a000f0d8
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-05 13:46:04 +0100

    Initialy a user is not registered

--- specs/user_registration_spec.py
+++ specs/user_registration_spec.py
@@ -1,5 +1,5 @@
 
-from expects import expect, be_true, raise_error
+from expects import expect, be_true, be_false, raise_error
 
 class UserService(object):
     def __init__(self):
@@ -20,12 +20,19 @@ with description('Register user'):
     with before.each:
         self.nickname = '@foolano'
 
+    with it('initialy a user is not registered'):
+        user_service = create_user_service()
+
+        expect(user_service.is_registered('irrelevant_ninckname')).to(be_false)
+
     with it('registers a new user'):
         user_service = create_user_service()
 
         user_service.register(self.nickname)
 
         expect(user_service.is_registered(self.nickname)).to(be_true)
+
+
 
     with _context('when user already exists'):
         with it('raises error'):
