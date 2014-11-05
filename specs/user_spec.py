afc6dd8756e3219971ece92a643c9284c5a6ff28
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-05 15:55:07 +0100

    Refactor

--- specs/user_spec.py
+++ specs/user_spec.py
@@ -1,8 +1,29 @@
 from co import co
-from expects import expect, be_empty, contain_exactly, be_false, raise_error
+from expects import expect, be_empty, be_true, contain_exactly, be_false, raise_error
 
 
-with description('User'):
+with description('Register user'):
+
+    with before.each:
+        self.nickname = '@foolano'
+        self.user_service = co.create_user_service()
+
+    with it('initialy a user is not registered'):
+        expect(self.user_service.is_registered('irrelevant_ninckname')).to(be_false)
+
+    with it('registers a new user'):
+        self.user_service.register(self.nickname)
+
+        expect(self.user_service.is_registered(self.nickname)).to(be_true)
+
+    with context('when user already exists'):
+        with it('raises error'):
+            self.user_service.register(self.nickname)
+
+            expect(lambda: self.user_service.register(self.nickname)).to(raise_error(co.UserAlreadyRegisteredError))
+
+
+with description('User followers'):
 
     with before.each:
         self.nickname = '@foolano'
