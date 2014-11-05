f7a51516f7224058bb0a7ada6868fd2a9af8f405
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-05 14:25:51 +0100

    Support for adding followers

--- co/co.py
+++ co/co.py
@@ -8,6 +8,7 @@ class UserAlreadyRegisteredError(Exception):
 class UserService(object):
     def __init__(self):
         self._users = set()
+        self._followers = {}
 
     def register(self, nickname):
         if self.is_registered(nickname):
@@ -15,7 +16,10 @@ class UserService(object):
         self._users.add(nickname)
 
     def is_registered(self, nickname):
-        return nickname in self._users
+		return nickname in self._users
 
     def followers_for(self, nickname):
-    	return []
+		return self._followers.get(nickname, [])
+
+    def add_follower(self, nickname, follower):
+		self._followers.setdefault(nickname, []).append(follower)
